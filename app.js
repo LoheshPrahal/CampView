9c56b1131669d3c0d741e6f973ecc8ed63b30dae
LoheshPrahal <loheshp@outlook.com>
2025-02-17 20:43:55 +0530

    Working on bootstrap

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
 const db = mongoose.connection;
@@ -17,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ urlencoded : true}))
 app.use(methodOverride('_method'));
-
+app.engine('ejs', ejsMate);
 app.get('/', (req, res) => {
     res.render('home');
 })
